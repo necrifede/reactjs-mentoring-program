8beff334598b992ec71720ed70b502477ef1d5e1
Nestor Felipe <necrifede@gmail.com>
2022-06-07 10:40:55 +0200

    Core concepts task (#1)
    * Core concepts task
    
    for this concepts a folder components were created inside a create-react-app
    
    this folder holds the 3 types of creating components
    
    * #1 testing empty values for jsx

---
+++ src/components/ClassComp.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class ClassComp extends Component {
+    render() {
+        return <div>This is a Class component</div>;
+    }
+}
+
+export default ClassComp;
