2e1afba240fef364dce0d59b4ecdb85cf0b6fb6f
Nestor Felipe <necrifede@gmail.com>
2022-06-21 02:58:50 +0200

    #2 update yarn.lock and add node version

--- webpack.development.js
+++ webpack.development.js
@@ -1,5 +1,4 @@
 const path = require("path");
-let HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     mode: "development",
@@ -7,34 +6,20 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "[name].bundle.js",
-        // publicPath: path.resolve(__dirname, "public"),
     },
     resolve: { extensions: [".js", ".jsx"] },
     module: {
         rules: [
             {
-                // test: /(\.js|\.jsx)$/,
                 test: /\.jsx?$/,
                 loader: "babel-loader",
-                exclude: [/node_modules/], //, /^{projects}$/
+                exclude: [/node_modules/],
                 options: { presets: ["@babel/preset-react"] },
             },
-            // {
-            //     test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
-            //     exclude: [/projects/, /node_modules/],
-            //     use: ["file-loader?name=[name].[ext]"], // ?name=[name].[ext] is only necessary to preserve the original file name
-            // },
         ],
     },
-    // plugins: [
-    //     new HtmlWebpackPlugin({
-    //         template: "./public/index.html",
-    //         filename: "./index.html",
-    //     }),
-    // ],
     infrastructureLogging: {
         colors: true,
-        // level: "verbose",
     },
     stats: {
         errorDetails: true,
